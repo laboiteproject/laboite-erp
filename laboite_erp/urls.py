90372c9a9a35bbf84e105943b89a73028251cea1
Julien Lebunetel <julien@lebunetel.com>
2017-02-13 11:57:56 +0100

    accounting models creation and activation

--- laboite_erp/urls.py
+++ laboite_erp/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^accounting/', include('accounting.urls')),
+    url(r'^accounting/', include('laboite_erp.accounting.urls')),
     url(r'^admin/', admin.site.urls),
 ]

6e519521d17de80bd495f70a8534da5bab4f9fed
Julien Lebunetel <julien@lebunetel.com>
2017-02-13 14:12:11 +0100

    accounting app admin configuration

--- laboite_erp/accounting/admin.py
+++ laboite_erp/accounting/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Journal, Entry, Record
+
+admin.site.register(Journal)
+admin.site.register(Entry)
+admin.site.register(Record)
--- laboite_erp/accounting/models.py
+++ laboite_erp/accounting/models.py
@@ -3,12 +3,21 @@ from django.db import models
 
 class Journal(models.Model):
     creation_date = models.DateTimeField('creation date', auto_now_add=True)
+    title = models.CharField(max_length=200, default='journal name')
+
+    def __str__(self):
+        return self.title
+
 
 class Entry(models.Model): # Écriture en Français
     journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
     creation_date = models.DateTimeField('creation date', auto_now_add=True)
     ref_date = models.DateField('transaction date')
     explanation = models.CharField(max_length=200) # libellé en Français
+
+    def __str__(self):
+        return "{0} {1}".format(self.ref_date, self.explanation)
+
 
 class Record(models.Model):
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
@@ -27,3 +36,10 @@ class Record(models.Model):
 
     def is_credit(self):
         return self.side == self.CREDIT
+
+    def __str__(self):
+        if self.is_debit():
+            message = "{0} | {1} | -"
+        else:
+            message = "{0} | - | {1}"
+        return message.format(self.account, self.amount)
